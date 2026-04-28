c874c5345e2565654ef48ff944aa882f5fe67e2d
SVVVLYXV <dsaval89@gmail.com>
2026-04-28 19:28:03 +0300

    style(ui): fix style for main dashbord

--- EduDev Tracker/Features/Dashboard/Views/DashboardPage.xaml.cs
+++ EduDev Tracker/Features/Dashboard/Views/DashboardPage.xaml.cs
@@ -2,8 +2,24 @@ namespace EduDev_Tracker.Features.Dashboard.Views;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage()
-	{
-		InitializeComponent();
-	}
+
+    const double WidthThreshold = 900;
+    public DashboardPage()
+    {
+        InitializeComponent();
+
+        this.SizeChanged += DashboardPage_SizeChanged;
+    }
+
+    private void DashboardPage_SizeChanged(object? sender, EventArgs e)
+    {
+        if (Width < WidthThreshold)
+        {
+            VisualStateManager.GoToState(CardsGrid, "Narrow");
+        }
+        else
+        {
+            VisualStateManager.GoToState(CardsGrid, "Wide");
+        }
+    }
 }
